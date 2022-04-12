46f037324e47976de5045f1bed23b3f3cbfc072a
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-12 11:56:27 -0500

    finished app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app" class="app">
     <div class="app__heading">
-      <img alt="Prefect logo" class="app__heading-logo" src="./assets/logo.svg" />
+      <img alt="Prefect logo" class="app__heading-logo" src="./assets/logo.svg">
       <h2 class="text-h2">
         Front End Software Engineer Challenge
       </h2>
 
       <div class="app__navigation">
-        <router-link to="/" exact>Instructions</router-link>
-        <span class="nav-divider" />
-        <router-link to="/exercise-1">Exercise #1</router-link>
-        <span class="nav-divider" />
-        <router-link to="/exercise-2">Exercise #2</router-link>
-        <span class="nav-divider" />
-        <router-link to="/exercise-3">Exercise #3</router-link>
+        <div class="app__navigation-item">
+          <router-link to="/" exact>
+            Instructions
+          </router-link>
+        </div>
+        <div class="app__navigation-item">
+          <router-link to="/exercise-1">
+            Exercise #1
+          </router-link>
+        </div>
+        <div class="app__navigation-item">
+          <router-link to="/exercise-2">
+            Exercise #2
+          </router-link>
+        </div>
+        <div class="app__navigation-item">
+          <router-link to="/exercise-3">
+            Exercise #3
+          </router-link>
+        </div>
       </div>
     </div>
 
@@ -37,5 +50,30 @@
 
 .app__heading-logo {
   max-width: 200px;
+}
+
+.app__navigation {
+  display: flex;
+  justify-content: center;
+}
+
+.app__navigation-item {
+  .router-link-active {
+    color: #2c3e50;
+    text-decoration: none;
+  }
+
+  &::after {
+    content: "|";
+    padding: 0 8px;
+  }
+  &:last-child::after {
+    content: none;
+  }
+}
+
+a {
+  color: #27b1ff;
+  font-weight: bold;
 }
 </style>
