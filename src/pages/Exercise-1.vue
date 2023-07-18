7d1cb07e2cf5415528a00a9b514562c6b3cc52a9
nicholas <znicholasbrown@gmail.com>
2023-07-18 10:45:25 -0400

    Fix a typo

--- src/pages/Exercise-1.vue
+++ src/pages/Exercise-1.vue
@@ -4,8 +4,8 @@
       Exercise 1: Python
     </p-heading>
 
-    <p>The following questions should take you no longer than 2 hours to complete; the goal is to get a sense for how you communicate technical concepts and write pre - these questions are not designed to trick you.  Please explain your thought process as much as possible.</p>
-    <p>This exercise is not timed, however, the sooner we receive it back the sooner we can move forward in our interview process. Please add your answers to your cloned repo in GitHub in whatever format is easiest for you (e.g. a Jupiter notebook). </p>
+    <p>The following questions should take you no longer than 2 hours to complete; the goal is to get a sense for how you communicate technical concepts and write code - these questions are not designed to trick you.  Please explain your thought process as much as possible.</p>
+    <p>This exercise is not timed, however, the sooner we receive it back the sooner we can move forward in our interview process. Please add your answers to your cloned repository in GitHub in whatever format is easiest for you (e.g. a Jupiter notebook). </p>
     <p>
       <p-link href="https://gist.github.com/zhen0/70828dd54ad673b5e29b087d2053a11f">
         Click here
