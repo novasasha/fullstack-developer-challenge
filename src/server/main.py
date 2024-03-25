ff723b75036e32b6edfcf341c2982bbeedc7838b
nicholas <znicholasbrown@gmail.com>
2024-03-25 12:27:49 -0400

    Add uvicorn server wrap to __main__

--- src/server/main.py
+++ src/server/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        port=8000,
+    )
